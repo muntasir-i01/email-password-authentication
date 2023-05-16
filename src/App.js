5c747756f9d5a7f09fee9d8e51c4e64b4726a549
muntasir-i01 <muntasirm55@gmail.com>
2023-05-16 11:25:43 +0600

    handle email verification and reset password link from firebase->documents->web->manage users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import app from './firebase.init';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
@@ -52,19 +52,55 @@ function App() {
     setValidated(true);
     setError('');
 
-    //console.log('Form submitted', email, password);
-    createUserWithEmailAndPassword(auth, email, password)
+    if(register) {
+      console.log(email, password);
+      signInWithEmailAndPassword(auth, email, password)
+      .then(userCredential => {
+        const user = userCredential.user;
+        console.log(user);
+      })
+
+      .catch(error => {
+        console.error(error);
+        setError(error.message);
+      })
+    }
+
+    else {
+      createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
         const user = userCredential.user;
         console.log(user);
         setEmail('');
         setPassword('');
+        verifyEmail();
     })
     .catch((error) => {
       console.log(error);
       setError(error.message);
     })
+    }
+
+    //console.log('Form submitted', email, password);
+    
     event.preventDefault();
+  }
+
+  const verifyEmail = () => {
+    sendEmailVerification(auth.currentUser)
+    .then(() => {
+      console.log('Email Verification Sent');
+    })
+  }
+
+  const handleForgotPassword = () => {
+    sendPasswordResetEmail(auth, email)
+    .then(() => {
+      console.log('Password reset email sent');
+    })
+    .catch((error) => {
+      console.log('Error on sending password reset email');
+    })
   }
 
   return (
@@ -87,9 +123,12 @@ function App() {
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check onChange={handleRegisterChange} type="checkbox" label="Already Registered?" />
           </Form.Group>
+
+          <Button onClick={handleForgotPassword} variant="link">Forgot Password?</Button> <br></br>
+          
           <p className='text-danger'>{error}</p>
           <Button variant="primary" type="submit">
-            Register
+            {register ? 'Login' : 'Register'}
           </Button>
         </Form>
       </div>
